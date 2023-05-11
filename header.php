d18108f709a2bcf87aefa5c90b2b23fa842bd167
dev LN <dev.ckln@gmail.com>
2023-05-11 12:17:45 +0530

    Initial commit

---
+++ header.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Yucanatis</title>
+  <link href="https://kit-pro.fontawesome.com/releases/v5.15.3/css/pro.min.css" rel="stylesheet">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/slick.css" rel="stylesheet">
+  <link href="css/carouselTicker.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+
+<body>
+  <div class="topbar">
+    <p class="marquee_text"><span class="bright-green">Welcome</span> <span class="white-color">to</span> <span class="red-color">Mexico!</span> Feel free to browse east-coast Yucatan. | <b>Our offer and performance are
+        being improved.</b> | Click the <b>Join the Waitlist buttons, enquire, book without a deposit and get early
+        access to discounted prices.</b> Plan ahead, enjoy efficient <b>customer-service.</b> Thank you for kindly
+      waiting…. | Yucatanis ribbon cutting ceremony is on July ??, 2023. | Yucatanis Yours.</p>
+  </div>
+  <header>
+    <div class="main-nav">
+      <div class="top-header-bar">
+        <div>
+          <p class="font14">very mucho Mexico</p>
+        </div>
+        <div class="right-nav">
+          <a class="" href="#">
+            <svg width="27" height="24" viewBox="0 0 27 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.142578 16.673C0.142578 19.5261 2.68628 21.6743 5.83045 21.6743C8.96455 21.6743 11.4983 19.5261 11.4983 16.673V5.6424C11.4983 3.69575 10.1957 2.41064 8.18934 2.41064C6.6067 2.41064 5.35977 3.21924 4.75569 4.64437L0.647623 14.3169C0.257488 15.2278 0.142578 15.947 0.142578 16.673ZM3.55352 12.1805L6.46708 5.33182C6.77424 4.60599 7.38315 4.18814 8.11642 4.18814C9.05644 4.18814 9.67075 4.81171 9.67075 5.74706V13.0899C8.697 12.204 7.30622 11.6736 5.83045 11.6736C5.04676 11.6736 4.2466 11.8585 3.55352 12.1805ZM2.04141 16.673C2.04141 14.7824 3.66884 13.5003 5.83045 13.5003C7.98383 13.5003 9.66252 14.7824 9.66252 16.673C9.66252 18.5618 7.98383 19.8457 5.83045 19.8457C3.66884 19.8457 2.04141 18.5618 2.04141 16.673ZM26.1698 16.6784C26.1698 15.9506 26.0467 15.2314 25.6583 14.3224L21.5567 4.64798C20.9444 3.22286 19.7076 2.41426 18.1231 2.41426C16.1168 2.41426 14.8142 3.70118 14.8142 5.64782V16.6784C14.8142 19.5315 17.3478 21.6779 20.482 21.6779C23.6179 21.6779 26.1698 19.5315 26.1698 16.6784ZM22.7589 12.1841C22.0658 11.8621 21.2657 11.6773 20.482 11.6773C19.0062 11.6773 17.6072 12.2094 16.6335 13.0935V5.75249C16.6335 4.81532 17.256 4.19174 18.196 4.19174C18.921 4.19174 19.5317 4.60961 19.8371 5.33544L22.7589 12.1841ZM24.2627 16.6784C24.2627 18.5654 22.6354 19.8493 20.482 19.8493C18.3203 19.8493 16.65 18.5654 16.65 16.6784C16.65 14.7878 18.3203 13.5039 20.482 13.5039C22.6354 13.5039 24.2627 14.7878 24.2627 16.6784ZM10.9012 14.5341C11.4784 14.2004 12.297 13.9865 13.1612 13.9865C14.1966 13.9865 14.8927 14.1409 15.458 14.5159V12.8438C14.815 12.5861 14.0558 12.4663 13.1612 12.4663C12.3618 12.4663 11.5808 12.6208 10.9012 12.8905V14.5341ZM10.9012 11.456C11.4784 11.1141 12.297 10.9084 13.1612 10.9084C14.1966 10.9084 14.8927 11.0546 15.458 11.4378V9.76754C14.815 9.50979 14.0558 9.39006 13.1612 9.39006C12.3618 9.39006 11.5808 9.54455 10.9012 9.81415V11.456ZM10.9012 8.3643C11.4784 8.03061 12.297 7.81666 13.1612 7.81666C14.1966 7.81666 14.8927 7.97295 15.458 8.34602V6.68403C14.815 6.42629 14.0558 6.29832 13.1612 6.29832C12.3618 6.29832 11.5808 6.46286 10.9012 6.72884V8.3643Z" fill="#FFCC01" />
+            </svg>
+            Last minute booking</a>
+          <div class="divider"></div>
+          <a class="green-color" href="">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12.0914 17.3808C15.0058 17.3808 17.3875 15.0026 17.3875 12.0883C17.3875 9.17209 15.0058 6.79211 12.0896 6.79211C9.17522 6.79211 6.79705 9.17209 6.79705 12.0883C6.79705 15.0026 9.17883 17.3808 12.0914 17.3808ZM12.0914 15.5693C10.1672 15.5693 8.60849 14.0106 8.60849 12.0883C8.60849 10.1623 10.1672 8.60176 12.0914 8.60176C14.0174 8.60176 15.5761 10.1623 15.5761 12.0883C15.5761 14.0106 14.0192 15.5693 12.0914 15.5693ZM4.1979 17.8418L6.40089 20.0448L10.3362 16.1114L8.13319 13.9065L4.1979 17.8418ZM4.09926 6.31921L8.13319 10.3532L10.3362 8.15015L6.30406 4.11622L4.09926 6.31921ZM20.1018 6.33568L17.8988 4.13269L13.8814 8.15015L16.0843 10.3532L20.1018 6.33568ZM20.0114 17.8254L16.0926 13.9065L13.8814 16.1196L17.8001 20.0366L20.0114 17.8254ZM12.0896 22.4658C17.7741 22.4658 22.4671 17.7663 22.4671 12.0901C22.4671 6.4056 17.7658 1.71436 12.0814 1.71436C6.40692 1.71436 1.71387 6.4056 1.71387 12.0901C1.71387 17.7663 6.41516 22.4658 12.0896 22.4658ZM12.0896 20.4219C7.46602 20.4219 3.76597 16.7136 3.76597 12.0901C3.76597 7.46651 7.45779 3.75823 12.0814 3.75823C16.7049 3.75823 20.4232 7.46651 20.4232 12.0901C20.4232 16.7136 16.7131 20.4219 12.0896 20.4219Z" fill="#0FBB69" />
+            </svg>
+            4 min tourist advice</a>
+        </div>
+
+      </div>
+      <div class="header-bar sticky-top">
+        <div class="header-logo">
+          <a href="">
+            <img src="images/main-logo.svg">
+          </a>
+          <!-- <div class="divider"></div> -->
+        </div>
+
+        <div class="header-nav">
+          <div class="primary-nav">
+            <ul>
+              <li><a href="">SHOP Shared Experiences</a></li>
+              <li><a href="">SHOP Private Experiences</a></li>
+              <li><a href="">Essential Rarities</a></li>
+              <li><a href="">Why Yucatanis</a></li>
+            </ul>
+          </div>
+          <div class="toggle-menu">
+            <div class="hamburger" id="menuToggle">
+              <span class="line"></span>
+              <span class="line"></span>
+              <span class="line"></span>
+            </div>
+            <nav class="fullscreen">
+              <div class="hamberger-topbar">
+                <div class="header-logo">
+                  <a href="">
+                    <img src="images/main-logo.svg">
+                  </a>
+                  <div class="divider"></div>
+                  <p>very mucho Mexico</p>
+                </div>
+                <div class="close">
+                  <div></div>
+                </div>
+              </div>
+              <ul>
+                <li><a href="/">At Your Service</a></li>
+                <li><a href="/">Get a Free Quote</a></li>
+                <li><a href="/">Practical Information</a></li>
+                <li><a href="/">About Us</a></li>
+                <li><a href="/">Add Us To Your Address Book</a></li>
+                <li><a href="/">Contact</a></li>
+                <li><a href="/">Bird’s-Eye View</a></li>
+              </ul>
+
+            </nav>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </header>
---
+++ listing.php
@@ -0,0 +1,91 @@
+<?php include('header.php') ?>
+<section class="listing-banner banner">
+  <img class="bg-img" class="img-fluid" src="images/listning-banner.png" alt="banner">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="banner">
+          <div class="bg">
+            <div class="content">
+              <h1 class="white-color"><span class="orange-color">Explore</span> Experiences</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="banner">
+          <form>
+            <div class="distance-field">
+              <div class="d-grid">
+                <label class="is-vishidden font14">From</label>
+                <input type="search" placeholder="" />
+              </div>
+              <div class="border-right"></div>
+              <img src="images/uil_exchange.png" class="img-fluid exchange-icon" alt="exchange  ">
+              <div class="d-grid">
+                <label class="is-vishidden">To</label>
+                <input type="search" placeholder="" />
+              </div>
+              <div class="border-right"></div>
+            </div>
+            <div class="slect-items-wrapper">
+              <div class="select-items">
+                <label class="" for="Date Range">Date Range</label>
+                <select class="options" name="date-range">
+                  <option value="0" selected="">Choose</option>
+                  <option value="0">High Priority</option>
+                  <option value="1">High Priority</option>
+                  <option value="2">High Priority</option>
+                </select>
+              </div>
+              <div class="border-right"></div>
+              <div class="select-items">
+                <label class="" for="Traveller(s)">Traveller(s)</label>
+                <select class="options" name="traveller">
+                  <option value="0" selected="">Choose</option>
+                  <option value="0">4 Travellers</option>
+                  <option value="1">6 Travellers</option>
+                  <option value="2">8 Travellers</option>
+                </select>
+              </div>
+            </div>
+            <input type="submit" class="btn btn-secondary" value="Explore Now">
+          </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</section>
+<section class="listing-block ptb-100-90">
+  <div class="container">
+    <h2 class="orange-color">
+      Islands,<span class="oddgreendark">National Parks</span> , Mild Water Pursuits
+    </h2>
+    <div class="row">
+      <div class="col-12 col-md-4"></div>
+      <div class="col-12 col-md-8">
+        <div class="listing-card">
+          <div class="card">
+            <img src="images/BestSnorkelingSpotCancunReef.png" class="img-fluid" alt="Snorkeling">
+            <div class="card-body">
+              <h6>Best Snorkeling Spot Cancun Reef</h6>
+              <div>
+
+              </div>
+              <p class="font16">With its rich legacy of Aztec, Mayan and Spanish Colonial architecture, Mexico is
+                perhaps
+                unsurprisingly home to more...</p>
+              <div class="card-footer">
+                <p class="">$2217.41</p>
+                <a class="btn btn-secondary">Book Experience!</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include('footer.php') ?>
